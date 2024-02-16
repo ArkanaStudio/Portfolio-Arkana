2828eb44d7dba8d889b56496840907929850e55e
ArkanaStudio <dwika820@gmail.com>
2024-02-16 08:39:58 +0700

    darkmode

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ var typed = new Typed(".auto-type", {
     backSpeed: 100,
     loop: true
 })
+
+
+let icon = document.getElementById("icon");
+
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "images/sun.png";
+    } else {
+        icon.src = "images/moon.png";
+    }
+}
